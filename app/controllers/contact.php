7e19d553b1cdb87af20caaa12cb017987a24ed78
abdelghafourettaqui1 <ettaqui88@gmail.com>
2022-02-13 18:12:49 +0100

    mvc

---
+++ app/controllers/contact.php
@@ -0,0 +1,12 @@
+<?php
+
+class contact extends controller {
+    public function index (){
+        echo 'contact index';
+    }
+    public function test (){
+        echo 'contact by phone';
+    }
+}
+
+?>
